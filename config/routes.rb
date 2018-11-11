0b2567af0e59e540195b558b6178e7f238fa1f87
namanmanchanda09 <naman.mohan09@outlook.com>
2018-11-11 23:03:26 +0530

    Adding the controllers and the Views

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
   get 'about', to:'pages#about'
 
+  resources :articles
+
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
